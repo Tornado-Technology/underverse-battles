832d8444e0b20ccb14d56b48b353ca47adb522e8
Tornado-Technology <tornado.the.technologies@gmail.com>
2023-05-12 23:50:21 +1000

    Fixed import crushes

--- server/src/util/matchmaker.ts
+++ server/src/util/matchmaker.ts
@@ -1,7 +1,7 @@
 import Client, { state } from '../concepts/client.js';
 import App from '../app.js';
 import Fight from '../game/fight/fight.js';
-import Logger from "./logging";
+import Logger from './logging.js';
 
 export default class Matchmaker {
   public static makeMatch(client1: Client, client2: Client): void {
