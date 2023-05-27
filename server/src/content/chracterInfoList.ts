c3732a1bce883f6528f79c9138ea9b11d8e3c122
Max1511 <43958870+Max1511@users.noreply.github.com>
2023-05-27 23:43:07 +0300

    Changed character info

--- server/src/content/chracterInfoList.ts
+++ server/src/content/chracterInfoList.ts
@@ -40,7 +40,7 @@ export const characterInfoList = {
   swapSans: new CharacterInfo(
     4,
     120,
-    100,
+    120,
     100,
     25,
     [10, 15, 20],
@@ -66,7 +66,7 @@ export const characterInfoList = {
   ),
   cross: new CharacterInfo(
     7,
-    120,
+    100,
     100,
     100,
     20,
@@ -77,7 +77,7 @@ export const characterInfoList = {
     [
       new CharacterInfo(
         7,
-        100,
+        120,
         100,
         100,
         20,
