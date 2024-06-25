7d6080b3df64f4f0222b9421118a9b7f84387c5d
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-06-26 01:19:20 +1000

    Updated fromating

--- server/src/api/command.ts
+++ server/src/api/command.ts
@@ -1,40 +1,40 @@
 import { IncomingMessage, ServerResponse } from 'http';
 
-type ApiCommandCallback = (
-        cmd: ApiCommand,
-        req: IncomingMessage,
-        res: ServerResponse,
-        url: URL,
-        method: string | undefined,
-    ) => void;
+type apiCommandCallback = (
+    cmd: ApiCommand,
+    req: IncomingMessage,
+    res: ServerResponse,
+    url: URL,
+    method: string | undefined,
+  ) => void;
 
 export class ApiCommand {
-    public readonly id: string; 
-    public readonly access: ApiCommandAccess;
-    public readonly callback: ApiCommandCallback; 
+  public readonly id: string; 
+  public readonly access: apiCommandAccess;
+  public readonly callback: apiCommandCallback; 
 
-    constructor(id: string, access: ApiCommandAccess, callback: ApiCommandCallback) {
-        this.id = id;
-        this.access = access;
-        this.callback = callback;
-    }
+  constructor(id: string, access: apiCommandAccess, callback: apiCommandCallback) {
+    this.id = id;
+    this.access = access;
+    this.callback = callback;
+  }
 
-    public run(req: IncomingMessage, res: ServerResponse, url: URL) {
-        this.callback(this, req, res, url, this.getMethod(req));
-    }
+  public run(req: IncomingMessage, res: ServerResponse, url: URL) {
+    this.callback(this, req, res, url, this.getMethod(req));
+  }
 
-    public send(res: ServerResponse, data: object) {
-        res.statusCode = 200;
-        res.write(JSON.stringify(data));
-        res.end();
-    }
+  public send(res: ServerResponse, data: object) {
+    res.statusCode = 200;
+    res.write(JSON.stringify(data));
+    res.end();
+  }
 
-    protected getMethod(req: IncomingMessage): string | undefined {
-        return req.method;
-    }
+  protected getMethod(req: IncomingMessage): string | undefined {
+    return req.method;
+  }
 }
 
-export enum ApiCommandAccess {
-    Guest,
-    Authorized,
+export enum apiCommandAccess {
+  Guest,
+  Authorized,
 }
--- server/src/api/server.ts
+++ server/src/api/server.ts
@@ -1,88 +1,88 @@
 import { IncomingMessage, Server, ServerResponse, createServer } from 'http';
-import { ApiCommand, ApiCommandAccess } from './command.js';
+import { ApiCommand, apiCommandAccess } from './command.js';
 
 export class ApiServer {
-    private readonly ip: string;
-    private readonly port: number;
-    private readonly commands: Map<string, ApiCommand>;
+  private readonly ip: string;
+  private readonly port: number;
+  private readonly commands: Map<string, ApiCommand>;
 
-    private instance?: Server;
+  private instance?: Server;
 
-    constructor(ip: string, port: number) {
-        this.ip = ip;
-        this.port = port;
-        this.commands = new Map<string, ApiCommand>();
-    }
-    
-    /**
-     * Instantiate HTTP server and binding listener.
-     */
-    public run() {
-        if (this.instance) {
-            throw new Error('Multiply ApiServer run');
-        } 
+  constructor(ip: string, port: number) {
+    this.ip = ip;
+    this.port = port;
+    this.commands = new Map<string, ApiCommand>();
+  }
+  
+  /**
+   * Instantiate HTTP server and binding listener.
+   */
+  public run() {
+    if (this.instance) {
+      throw new Error('Multiply ApiServer run');
+    } 
 
-        this.instance = createServer(this.listener.bind(this));
-        this.instance.listen(this.port, this.ip);
-    }
+    this.instance = createServer(this.listener.bind(this));
+    this.instance.listen(this.port, this.ip);
+  }
 
-    public registerCommand(command: ApiCommand) {
-        if (this.commands.has(command.id)){
-            throw new Error(`Command Id ${command.id} already registered`);
-        }
-
-        this.commands.set(command.id, command);
+  public registerCommand(command: ApiCommand) {
+    if (this.commands.has(command.id)){
+      throw new Error(`Command Id ${command.id} already registered`);
     }
 
-    private listener(req: IncomingMessage, res: ServerResponse) {
-        const url = new URL(req.url, `http://${req.headers.host}`);
-        const command = this.getCommand(url.pathname);
+    this.commands.set(command.id, command);
+  }
 
-        if (!command) {
-            res.statusCode = 404;
-            res.write(JSON.stringify({
-                error: 'No handler was found for the request',
-            }));
-            res.end();
-            return;
-        }
+  private listener(req: IncomingMessage, res: ServerResponse) {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const command = this.getCommand(url.pathname);
 
-        if (!this.hasAccess(command, url)) {
-            res.statusCode = 403;
-            res.write(JSON.stringify({
-                error: 'No access to this method',
-            }));
-            res.end();
-            return;
-        }
-
-        command.run(req, res, url);
+    if (!command) {
+      res.statusCode = 404;
+      res.write(JSON.stringify({
+        error: 'No handler was found for the request',
+      }));
+      res.end();
+      return;
     }
 
-    private getCommand(id: string): ApiCommand | undefined {
-        if (!this.commands.has(id))
-            return undefined;
-
-        return this.commands.get(id);
+    if (!this.hasAccess(command, url)) {
+      res.statusCode = 403;
+      res.write(JSON.stringify({
+          error: 'No access to this method',
+      }));
+      res.end();
+      return;
     }
 
-    private hasAccess(command: ApiCommand, url: URL): boolean {
-        switch (command.access) {
-            case ApiCommandAccess.Guest:
-                return true;
+    command.run(req, res, url);
+  }
 
-            case ApiCommandAccess.Authorized:
-                return this.hasToken(url.searchParams.get('token'));
+  private getCommand(id: string): ApiCommand | undefined {
+    if (!this.commands.has(id))
+      return undefined;
 
-            default:
-                throw new Error(`Unknown ApiCommandAccess ${command.access}`);
-        }
-    }
+    return this.commands.get(id);
+  }
 
-    private hasToken(token: string | null): boolean {
-        if (!token)
-            return false;
-
+  private hasAccess(command: ApiCommand, url: URL): boolean {
+    switch (command.access) {
+      case apiCommandAccess.Guest:
         return true;
+
+      case apiCommandAccess.Authorized:
+        return this.hasToken(url.searchParams.get('token'));
+
+      default:
+        throw new Error(`Unknown ApiCommandAccess ${command.access}`);
     }
+  }
+
+  private hasToken(token: string | null): boolean {
+    if (!token)
+      return false;
+
+    return true;
+  }
 }
